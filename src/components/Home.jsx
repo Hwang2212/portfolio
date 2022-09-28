bb155b620aa348168e358986e084a76d8385e94e
Hwang2212 <andyhwangsauyee@gmail.com>
2022-09-28 14:01:08 +0800

    Added Social Links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,16 +11,17 @@ const Home = () => {
                 <h2 className='text-4xl sm:text-7xl font-bold text-white py-4'>
                     I'm a Flutter Developer
                 </h2>
-                <p className='text-gray-500 py-4 max-w-md'>
+                <p className='text-gray-500 max-w-md'>
                     <FcLike/> solving problems using code. <br />
-                    Loves to build applications with Flutter, Node.js, Supabase.
-                    Self-learning on technologies such as React too! 
+                    Currently building applications with Flutter, Node.js, Supabase.
+                    Self-learning on technologies like React too! 
                 </p>
 
-                <div>
-                    <button>
+                <div> {/*Use group to to group hover */}
+                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-pink-500 cursor-pointer'>
                         Portfolio
-                        <RiArrowRightFill/>
+                        <span className='group-hover:rotate-90 duration-500'><RiArrowRightFill/></span>
+                        
                     </button>
                 </div>
             </div>
