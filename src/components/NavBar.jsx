98b3d641a7417a251559a428caa219adb660326c
Hwang2212 <andyhwangsauyee@gmail.com>
2022-10-08 16:18:11 +0800

    commiter

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
     return (
         <div className="flex justify-between items-center px-4 w-full h-20 text-white bg-black fixed">
             <div>
-                <h1 className='text-5xl font-signature  ml-2'>Andy</h1>
+                <h1 className='text-5xl font-signature  ml-2 tracking-wide duration-150 hover:tracking-widest'>Andy</h1>
             </div>
 
             <ul className='hidden md:flex'>
